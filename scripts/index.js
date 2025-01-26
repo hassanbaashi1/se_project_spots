7898180398c6da8c8ff93b560a5b82ba12eef5c9
Hassan <baashiyare11@gmail.COM>
2025-01-25 20:58:31 -0600

    project4 stag3

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,16 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal__opened");
+}
+function closeModal() {
+  editModal.classList.remove("modal__opened");
+}
+profileEditButton.addEventListener("click", openModal);
+editModalCloseBtn.addEventListener("click", closeModal);
